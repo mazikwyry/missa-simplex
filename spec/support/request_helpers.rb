444e1956b6806986c9bba2e32be13354357e9138
mazikwyry <a.mazur@exlabs.co.uk>
2018-03-01 22:28:10 +0100

    rails 5.2 ruby 2.5 devise + jwt basic scelleton

---
+++ spec/support/request_helpers.rb
@@ -0,0 +1,19 @@
+module RequestHelpers
+  def json
+    @json ||= JSON.parse(response.body)
+  end
+
+  def clear_json
+    @json = nil
+  end
+
+  def json_items_ids
+    json.map { |item| item['id'] }
+  end
+
+  def decoded_jwt_token_from_response(response)
+    token = response.headers['Authorization'].split(' ').last
+    hmac_secret = ENV['DEVISE_JWT_SECRET_KEY']
+    JWT.decode token, hmac_secret, true
+  end
+end
